ee38f94aaf7e1b595b5690fe2f07b181a155cc46
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-23 15:42:08 +0530

    Clean duplicate root backend files

--- src/main/java/com/ranihire/Skill2Intern/repository/CandidateProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ranihire.Skill2Intern.repository;
-
-import com.ranihire.Skill2Intern.model.CandidateProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateProfileRepository extends JpaRepository<CandidateProfile, Integer> {
-}
--- src/main/java/com/ranihire/Skill2Intern/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ranihire.Skill2Intern.repository;
-
-import com.ranihire.Skill2Intern.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUserName(String username);
-}
