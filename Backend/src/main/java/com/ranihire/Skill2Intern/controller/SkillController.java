6afa367c9e7a4bf2f3961aa6d20d8048b907a44d
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-23 20:35:02 +0530

    Implement Skill controller and get skills API

---
+++ Backend/src/main/java/com/ranihire/Skill2Intern/controller/SkillController.java
@@ -0,0 +1,22 @@
+package com.ranihire.Skill2Intern.controller;
+
+import com.ranihire.Skill2Intern.model.Skill;
+import com.ranihire.Skill2Intern.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class SkillController {
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("/skills")
+    public List<Skill> getAllSkills() {
+        return skillRepository.findAll();
+    }
+}
