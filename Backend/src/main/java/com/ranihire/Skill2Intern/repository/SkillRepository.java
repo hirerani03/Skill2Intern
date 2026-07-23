c4cbe00f4b2604c3e226ad7b0233b014aff97965
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-23 20:25:25 +0530

    Create Skill repository

---
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/SkillRepository.java
@@ -0,0 +1,7 @@
+package com.ranihire.Skill2Intern.repository;
+
+import com.ranihire.Skill2Intern.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillRepository extends JpaRepository<Skill, Integer> {
+}
