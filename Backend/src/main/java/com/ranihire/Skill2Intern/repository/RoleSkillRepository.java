363665af4f43a39dcb9c8de1cf04bdc66601c290
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 22:55:30 +0530

    Find recommended roles by skill IDs

--- Backend/src/main/java/com/ranihire/Skill2Intern/repository/RoleSkillRepository.java
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/RoleSkillRepository.java
@@ -1,10 +1,21 @@
 package com.ranihire.Skill2Intern.repository;
 
 
+import com.ranihire.Skill2Intern.model.Role;
 import com.ranihire.Skill2Intern.model.RoleSkill;
 import com.ranihire.Skill2Intern.model.RoleSkillId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface RoleSkillRepository extends JpaRepository<RoleSkill, RoleSkillId> {
+    @Query("""
+       SELECT DISTINCT rs.role
+       FROM RoleSkill rs
+       WHERE rs.skill.skillId IN :skillIds
+       """)
+    List<Role> findRecommendedRoles(@Param("skillIds") List<Integer> skillIds);
 }
