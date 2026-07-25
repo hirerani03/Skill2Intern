c6de2616eb129064b376149529726f92afd49e41
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 19:50:00 +0530

    Create Role repository

---
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.ranihire.Skill2Intern.repository;
+
+import com.ranihire.Skill2Intern.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
