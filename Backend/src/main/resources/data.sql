d906200d2529a7992cb70b392724c42bdbf4dbe2
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 20:16:04 +0530

    Add role master data

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -4,3 +4,15 @@ INSERT IGNORE INTO skill (skill_name) VALUES ('JavaScript');
 INSERT IGNORE INTO skill (skill_name) VALUES ('React');
 INSERT IGNORE INTO skill (skill_name) VALUES ('Java');
 INSERT IGNORE INTO skill (skill_name) VALUES ('AWS');
+
+       
+INSERT IGNORE INTO role (role_name) VALUES ('Java Developer');
+INSERT IGNORE INTO role (role_name) VALUES ('Backend Developer');
+INSERT IGNORE INTO role (role_name) VALUES ('Frontend Developer');
+INSERT IGNORE INTO role (role_name) VALUES ('Full Stack Developer');
+INSERT IGNORE INTO role (role_name) VALUES ('Software Engineer');
+INSERT IGNORE INTO role (role_name) VALUES ('Data Analyst');
+INSERT IGNORE INTO role (role_name) VALUES ('Data Engineer');
+INSERT IGNORE INTO role (role_name) VALUES ('Machine Learning Engineer');
+INSERT IGNORE INTO role (role_name) VALUES ('DevOps Engineer');
+INSERT IGNORE INTO role (role_name) VALUES ('Android Developer');
