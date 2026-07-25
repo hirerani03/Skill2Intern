9995e06e9fe71adc00315afebfc0661020117ee3
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 20:36:24 +0530

    Add role skill master data

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,11 +1,47 @@
+-- skill master data
 INSERT IGNORE INTO skill (skill_name) VALUES ('HTML');
 INSERT IGNORE INTO skill (skill_name) VALUES ('CSS');
 INSERT IGNORE INTO skill (skill_name) VALUES ('JavaScript');
 INSERT IGNORE INTO skill (skill_name) VALUES ('React');
 INSERT IGNORE INTO skill (skill_name) VALUES ('Java');
 INSERT IGNORE INTO skill (skill_name) VALUES ('AWS');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Spring Boot');
+INSERT IGNORE INTO skill (skill_name) VALUES ('MySQL');
+INSERT IGNORE INTO skill (skill_name) VALUES ('SQL');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Git');
+INSERT IGNORE INTO skill (skill_name) VALUES ('REST API');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Hibernate');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Docker');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Kubernetes');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Python');
+INSERT IGNORE INTO skill (skill_name) VALUES ('C++');
+INSERT IGNORE INTO skill (skill_name) VALUES ('C');
+INSERT IGNORE INTO skill (skill_name) VALUES ('DSA');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Node.js');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Express.js');
+INSERT IGNORE INTO skill (skill_name) VALUES ('MongoDB');
+INSERT IGNORE INTO skill (skill_name) VALUES ('TypeScript');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Redux');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Tailwind CSS');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Bootstrap');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Linux');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Jenkins');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Azure');
+INSERT IGNORE INTO skill (skill_name) VALUES ('GCP');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Power BI');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Excel');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Pandas');
+INSERT IGNORE INTO skill (skill_name) VALUES ('NumPy');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Scikit-learn');
+INSERT IGNORE INTO skill (skill_name) VALUES ('TensorFlow');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Spark');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Hadoop');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Firebase');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Android');
+INSERT IGNORE INTO skill (skill_name) VALUES ('XML');
+INSERT IGNORE INTO skill (skill_name) VALUES ('Jetpack Compose');
 
-       
+-- role master data
 INSERT IGNORE INTO role (role_name) VALUES ('Java Developer');
 INSERT IGNORE INTO role (role_name) VALUES ('Backend Developer');
 INSERT IGNORE INTO role (role_name) VALUES ('Frontend Developer');
@@ -16,3 +52,92 @@ INSERT IGNORE INTO role (role_name) VALUES ('Data Engineer');
 INSERT IGNORE INTO role (role_name) VALUES ('Machine Learning Engineer');
 INSERT IGNORE INTO role (role_name) VALUES ('DevOps Engineer');
 INSERT IGNORE INTO role (role_name) VALUES ('Android Developer');
+
+-- role_skill mappings
+-- Java Developer (role_id = 1)
+INSERT IGNORE INTO role_skill VALUES (1,5);
+INSERT IGNORE INTO role_skill VALUES (1,25);
+INSERT IGNORE INTO role_skill VALUES (1,26);
+INSERT IGNORE INTO role_skill VALUES (1,27);
+INSERT IGNORE INTO role_skill VALUES (1,28);
+INSERT IGNORE INTO role_skill VALUES (1,29);
+INSERT IGNORE INTO role_skill VALUES (1,30);
+
+-- Backend Developer (role_id = 2)
+INSERT IGNORE INTO role_skill VALUES (2,5);
+INSERT IGNORE INTO role_skill VALUES (2,25);
+INSERT IGNORE INTO role_skill VALUES (2,26);
+INSERT IGNORE INTO role_skill VALUES (2,27);
+INSERT IGNORE INTO role_skill VALUES (2,28);
+INSERT IGNORE INTO role_skill VALUES (2,29);
+INSERT IGNORE INTO role_skill VALUES (2,30);
+INSERT IGNORE INTO role_skill VALUES (2,31);
+INSERT IGNORE INTO role_skill VALUES (2,44);
+
+-- Frontend Developer (role_id = 3)
+INSERT IGNORE INTO role_skill VALUES (3,1);
+INSERT IGNORE INTO role_skill VALUES (3,2);
+INSERT IGNORE INTO role_skill VALUES (3,3);
+INSERT IGNORE INTO role_skill VALUES (3,4);
+INSERT IGNORE INTO role_skill VALUES (3,40);
+INSERT IGNORE INTO role_skill VALUES (3,41);
+INSERT IGNORE INTO role_skill VALUES (3,42);
+INSERT IGNORE INTO role_skill VALUES (3,43);
+INSERT IGNORE INTO role_skill VALUES (3,28);
+
+-- Full Stack Developer (role_id = 4)
+INSERT IGNORE INTO role_skill VALUES (4,1);
+INSERT IGNORE INTO role_skill VALUES (4,2);
+INSERT IGNORE INTO role_skill VALUES (4,3);
+INSERT IGNORE INTO role_skill VALUES (4,4);
+INSERT IGNORE INTO role_skill VALUES (4,5);
+INSERT IGNORE INTO role_skill VALUES (4,25);
+INSERT IGNORE INTO role_skill VALUES (4,26);
+INSERT IGNORE INTO role_skill VALUES (4,27);
+INSERT IGNORE INTO role_skill VALUES (4,28);
+INSERT IGNORE INTO role_skill VALUES (4,29);
+
+-- Software Engineer (role_id = 5)
+INSERT IGNORE INTO role_skill VALUES (5,5);
+INSERT IGNORE INTO role_skill VALUES (5,34);
+INSERT IGNORE INTO role_skill VALUES (5,35);
+INSERT IGNORE INTO role_skill VALUES (5,36);
+INSERT IGNORE INTO role_skill VALUES (5,28);
+
+-- Data Analyst (role_id = 6)
+INSERT IGNORE INTO role_skill VALUES (6,27);
+INSERT IGNORE INTO role_skill VALUES (6,33);
+INSERT IGNORE INTO role_skill VALUES (6,48);
+INSERT IGNORE INTO role_skill VALUES (6,49);
+INSERT IGNORE INTO role_skill VALUES (6,50);
+INSERT IGNORE INTO role_skill VALUES (6,51);
+
+-- Data Engineer (role_id = 7)
+INSERT IGNORE INTO role_skill VALUES (7,33);
+INSERT IGNORE INTO role_skill VALUES (7,27);
+INSERT IGNORE INTO role_skill VALUES (7,54);
+INSERT IGNORE INTO role_skill VALUES (7,55);
+INSERT IGNORE INTO role_skill VALUES (7,6);
+
+-- Machine Learning Engineer (role_id = 8)
+INSERT IGNORE INTO role_skill VALUES (8,33);
+INSERT IGNORE INTO role_skill VALUES (8,50);
+INSERT IGNORE INTO role_skill VALUES (8,51);
+INSERT IGNORE INTO role_skill VALUES (8,52);
+INSERT IGNORE INTO role_skill VALUES (8,53);
+
+-- DevOps Engineer (role_id = 9)
+INSERT IGNORE INTO role_skill VALUES (9,31);
+INSERT IGNORE INTO role_skill VALUES (9,32);
+INSERT IGNORE INTO role_skill VALUES (9,45);
+INSERT IGNORE INTO role_skill VALUES (9,44);
+INSERT IGNORE INTO role_skill VALUES (9,6);
+INSERT IGNORE INTO role_skill VALUES (9,46);
+INSERT IGNORE INTO role_skill VALUES (9,47);
+
+-- Android Developer (role_id = 10)
+INSERT IGNORE INTO role_skill VALUES (10,5);
+INSERT IGNORE INTO role_skill VALUES (10,57);
+INSERT IGNORE INTO role_skill VALUES (10,58);
+INSERT IGNORE INTO role_skill VALUES (10,56);
+INSERT IGNORE INTO role_skill VALUES (10,59);
