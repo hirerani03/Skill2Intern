3487fc5c2398b529ba927a0241d1548a61eeff84
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 20:11:56 +0530

    Create RoleSkill repository

---
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/RoleSkillRepository.java
@@ -0,0 +1,8 @@
+package com.ranihire.Skill2Intern.repository;
+
+import com.ranihire.Skill2Intern.model.RoleSkill;
+import com.ranihire.Skill2Intern.model.RoleSkillId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleSkillRepository extends JpaRepository<RoleSkill, RoleSkillId> {
+}
