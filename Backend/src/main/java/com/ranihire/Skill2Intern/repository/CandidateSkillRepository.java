be4aa9183903c2d0c852d455aaa80e8900fbc923
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-07-25 22:53:24 +0530

    Implement find skills by candidate ID query

--- Backend/src/main/java/com/ranihire/Skill2Intern/repository/CandidateSkillRepository.java
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/CandidateSkillRepository.java
@@ -3,6 +3,16 @@ package com.ranihire.Skill2Intern.repository;
 import com.ranihire.Skill2Intern.model.CandidateSkill;
 import com.ranihire.Skill2Intern.model.CandidateSkillId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CandidateSkillRepository extends JpaRepository<CandidateSkill, CandidateSkillId> {
+    @Query("""
+            SELECT cs.skill.skillId
+            FROM CandidateSkill cs
+            WHERE cs.candidateProfile.candidateId = :candidateId
+           """)
+
+    List<Integer> findSkillIdsByCandidateId(Integer candidateId);
 }
