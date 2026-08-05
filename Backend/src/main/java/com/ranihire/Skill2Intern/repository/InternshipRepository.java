a818863e4a0498b8544ef32fa10815dac9c6ead4
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-08-05 15:50:45 +0530

    Created Internship repository

---
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/InternshipRepository.java
@@ -0,0 +1,7 @@
+package com.ranihire.Skill2Intern.repository;
+
+import com.ranihire.Skill2Intern.model.Internship;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InternshipRepository extends JpaRepository<Internship, Integer> {
+}
