0800a58fc3773910d6b606091572caa8b91d224c
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-08-05 20:13:38 +0530

    Implement internship recommendation service

--- Backend/src/main/java/com/ranihire/Skill2Intern/service/InternshipService.java
+++ Backend/src/main/java/com/ranihire/Skill2Intern/service/InternshipService.java
@@ -1,32 +1,30 @@
 package com.ranihire.Skill2Intern.service;
 
 import com.ranihire.Skill2Intern.model.Internship;
-import com.ranihire.Skill2Intern.repository.InternshipRepository;
+import com.ranihire.Skill2Intern.repository.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class InternshipService {
+    private final CandidateSkillRepository candidateSkillRepository;
+    private final InternshipSkillRepository internshipSkillRepository;
     private final InternshipRepository internshipRepository;
 
-    public InternshipService(InternshipRepository internshipRepository) {
+    public InternshipService(CandidateSkillRepository candidateSkillRepository,
+                             InternshipSkillRepository internshipSkillRepository,
+                             InternshipRepository internshipRepository) {
+        this.candidateSkillRepository = candidateSkillRepository;
+        this.internshipSkillRepository = internshipSkillRepository;
         this.internshipRepository = internshipRepository;
     }
 
-    // Add Internship
-    public Internship addInternship(Internship internship) {
-        return internshipRepository.save(internship);
-    }
+    public List<Internship> getRecommendedInternships(Integer candidateId) {
 
-    // Get All Internships
-    public List<Internship> getAllInternships() {
-        return internshipRepository.findAll();
-    }
+        List<Integer> skillIds =
+                candidateSkillRepository.findSkillIdsByCandidateId(candidateId);
 
-    // Get Internship By Id
-    public Internship getInternshipById(Integer internshipId) {
-        return internshipRepository.findById(internshipId)
-                .orElseThrow(() -> new RuntimeException("Internship not found"));
+        return internshipSkillRepository.findRecommendedInternships(skillIds);
     }
 }
