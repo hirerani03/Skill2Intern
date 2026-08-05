65f74880623f0e4c893cb077421d5099316aba55
Rani Hire <rani_hire_comp@moderncoe.edu.in>
2026-08-05 20:02:28 +0530

    Implement internship recommendation repository

--- Backend/src/main/java/com/ranihire/Skill2Intern/repository/InternshipSkillRepository.java
+++ Backend/src/main/java/com/ranihire/Skill2Intern/repository/InternshipSkillRepository.java
@@ -1,8 +1,19 @@
 package com.ranihire.Skill2Intern.repository;
 
+import com.ranihire.Skill2Intern.model.Internship;
 import com.ranihire.Skill2Intern.model.InternshipSkill;
 import com.ranihire.Skill2Intern.model.InternshipSkillId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InternshipSkillRepository  extends JpaRepository<InternshipSkill, InternshipSkillId> {
+    @Query("""
+       SELECT DISTINCT ins.internship
+       FROM InternshipSkill ins
+       WHERE ins.skill.skillId IN :skillIds
+       """)
+    List<Internship> findRecommendedInternships(@Param("skillIds") List<Integer> skillIds);
 }
